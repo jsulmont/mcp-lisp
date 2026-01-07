36ae3599a5ff673fc80217943e11f98eb41c581b
Jan VL <me@janks.dev>
2026-01-07 17:15:32 +0100

    chore: emacs .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((lisp-mode . ((indent-tabs-mode . nil)
+               (fill-column . 100)
+               (lisp-indent-function . common-lisp-indent-function))))
